88ee1e55db56126120997071985448f6f88f4875
Ronan Ballantine <rballant@akamai.com>
2025-03-24 13:17:32 +0000

    muc process

--- approval_finder.py
+++ approval_finder.py
@@ -11,8 +11,6 @@ from ticket import Ticket
 
 
 class ApprovalFinder:
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    
     processed_tickets = []
     intel_data_strings = []
 
