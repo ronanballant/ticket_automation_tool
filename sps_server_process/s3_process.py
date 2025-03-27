e022c1d97c18b1fe5917de606158d43e62d02e46
Ronan Ballantine <rballant@akamai.com>
2025-03-27 10:55:02 +0000

    updates

--- sps_server_process/s3_process.py
+++ sps_server_process/s3_process.py
@@ -2,8 +2,8 @@ from s3_client import S3Client
 import csv
 import json
 from ioc_query import IocQuery
-from config import (destination_region, directory_prefix, empty_file_path, get_logger, results_path,
-                    search_fqdns_path, secops_s3_aws_access_key,
+from config import (destination_region, directory_prefix, empty_file_path, is_running_file, 
+                    get_logger, results_path, search_fqdns_path, secops_s3_aws_access_key,
                     secops_s3_aws_secret_key, secops_s3_bucket, secops_s3_endpoint, 
                     sps_intel_results_local_file, search_fqdns_local_file)
 
@@ -33,14 +33,15 @@ def get_domain_data(logger, domains):
 
 logger.info("Starting S3 Ticket Analyser")
 logger.info("Checking is_s3_running")
-with open("is_s3_running.csv", "r") as file:
+with open(is_running_file, "r") as file:
     lines = file.readlines()
 
     if "true" in lines[0]:
         logger.info(f"S3 Ticket Analyser currently running: {lines[0]}")
         exit()
     
-with open("is_s3_running.csv", "w") as file:
+logger.info("Writing true to is_s3_running.csv")
+with open(is_running_file, "w") as file:
     file.writelines("true")
 
 logger.info(f"Initialising S3 client")
@@ -60,7 +61,7 @@ data = s3_client.file_content.strip().split("\n")
 fqdns = [fqdn.strip().replace("[.]", ".") for fqdn in data]
 
 if fqdns[0] == "empty":
-    with open("is_s3_running.csv", "w") as file:
+    with open(is_running_file, "w") as file:
         file.writelines("false")
     logger.info(f"No FQDNs found... Exiting")
     exit()
@@ -88,5 +89,5 @@ s3_client.write_file(empty_file_path, search_fqdns_path)
 
 
 logger.info(f"Writing 'false' to is_s3_running.csv")
-with open("is_s3_running.csv", "w") as file:
+with open(is_running_file, "w") as file:
     file.writelines("false")
