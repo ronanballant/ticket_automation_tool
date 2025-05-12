cc0aa8cc8a9bb804355668d31924c69dd1b41b34
Ronan Ballantine <rballant@akamai.com>
2025-05-12 16:55:06 +0100

    update ticket creation

--- ticket_responder.py
+++ ticket_responder.py
@@ -352,8 +352,8 @@ class TicketResponder:
         tmp_dict["fields"]["components"] = [{"name": "Secops Automation"}]
         tmp_dict["fields"]["customfield_12703"] = "Internal"
         tmp_dict["fields"]["labels"] = ["ENT_SECOPS_OPERATIONS"]
-        tmp_dict["fields"]["customfield_10600"] = ["Low"]
-        
+        tmp_dict["fields"]["customfield_10600"] = ["20813"]
+
         json_object = json.dumps(tmp_dict, indent=4)
         try:
             response = requests.post(
