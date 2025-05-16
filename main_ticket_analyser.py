4406a1a21f55814f561e0ad8cd6eadc3720f6dba
Ronan Ballantine <rballant@akamai.com>
2025-05-16 10:35:16 +0100

    write s3 file

--- main_ticket_analyser.py
+++ main_ticket_analyser.py
@@ -88,10 +88,10 @@ def muc_server_process(fqdns, server_name, key_handler):
                 writer.writerow([fqdn])
 
         s3_client.write_file(search_fqdns_local_file, search_fqdns_path)
-        # s3_client.write_file(search_fqdns_local_file, results_path)
+        s3_client.write_file(search_fqdns_local_file, results_path)
 
         recheck = True
-        max_retries = 45
+        max_retries = 100
         retry_count = 0
         while recheck and retry_count < max_retries:
             time.sleep(60)
